cfd24e6ec96075488204aee286c10c25300038e2
Marek Sklodowski <masklod@gmail.com>
2012-05-15 04:29:45 +0200

    Working prototype with two buttons + two leds + two debug leds

--- logic.h
+++ logic.h
@@ -2,6 +2,7 @@
 #define STATE_SHOW_SEQUENCE 1
 #define STATE_SHOW_SEQUENCE_BREAK 2
 #define STATE_WAIT_FOR_PLAYER_REPEAT 3
+#define STATE_LOOSE 4
 
 volatile int gState = STATE_BEFORE_START;
 
@@ -9,6 +10,7 @@ volatile int gSequence[] = {0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0
 volatile int gLevel = 0; // Jak dluga sekwencje gracz bedzie musial powtorzyc
 volatile int gSeed = 0; // Ziarno losowania w tej grze.
 volatile int gShowingSequenceStep = 0; // Etap pokazywania sekwencji.
+volatile int gRepeatingSequenceStep = 0; // Etap powtatarzania przez gracza sekwencji.
 
 void NextStep()
 {
@@ -55,8 +57,9 @@ void NextStep()
 			{
 				// Jezeli pokazalismy cala sekwencje to konczymy.
 				P1OUT = 0;
+				gRepeatingSequenceStep = 0;
+				gTicksToNextStep = TIMER_SECOND;
 				gState = STATE_WAIT_FOR_PLAYER_REPEAT;
-				gTicksToNextStep = 0;
 			}
 		}
 		break;
@@ -64,9 +67,19 @@ void NextStep()
 		// Przed rozpoczeciem gry mruganie diodami
 		case STATE_WAIT_FOR_PLAYER_REPEAT:
 		{
+			P1OUT &= ~LED_0;
+			P1OUT &= ~LED_1;
 			P1OUT ^= LED_GREEN;
 			P1OUT ^= LED_RED;
-			gTicksToNextStep = TIMER_100MS * 3;
+			gTicksToNextStep = TIMER_100MS * 5;
+		}
+		break;
+
+		case STATE_LOOSE:
+		{
+			P1OUT &= ~LED_GREEN;
+			P1OUT ^= LED_RED;
+			gTicksToNextStep = TIMER_100MS;
 		}
 		break;
 	}
@@ -83,22 +96,40 @@ void ButtonPressed(int buttonNum)
 		// Dowolny przycisk rozpoczyna gre
 		case STATE_BEFORE_START:
 			// todo: losowanie tablicy
-			gLevel = 5;
+			gLevel = 0;
 			gShowingSequenceStep = 0;
 
 			gState = STATE_SHOW_SEQUENCE;
 			gTicksToNextStep = TIMER_SECOND;
 		break;
 
+		// Przed rozpoczeciem gry mruganie diodami
+		case STATE_WAIT_FOR_PLAYER_REPEAT:
+		{
+			if (buttonNum == 0)
+				P1OUT = LED_0;
+			else
+				P1OUT = LED_1;
 
-		case STATE_SHOW_SEQUENCE:
-			gShowingSequenceStep = 0;
+			if (buttonNum == gSequence[gRepeatingSequenceStep])
+			{
+				// Dobrze.
+				gTicksToNextStep = TIMER_100MS;
+				gRepeatingSequenceStep++;
+
+				if (gRepeatingSequenceStep > gLevel)
+				{
+					++gLevel;
+					gShowingSequenceStep = 0;
+					gState = STATE_SHOW_SEQUENCE_BREAK;
+				}
+			}
+			else
+			{
+				gState = STATE_LOOSE;
+				gTicksToNextStep = TIMER_SECOND * 5;
+			}
+		}
 		break;
 	}
-
-
-	//if (buttonNum == 0)
-	//	P1OUT ^= LED_0;
-	//else
-	//	P1OUT ^= LED_1;
 }
