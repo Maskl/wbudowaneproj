f29b13820d9cee4c07bbca96871bfac03c29fa09
Marek Sklodowski <masklod@gmail.com>
2012-05-15 02:40:37 +0200

    Code refactoring.

--- main.c
+++ main.c
@@ -1,78 +1,89 @@
 #include "msp430g2553.h"
-volatile int ticks = 0;
 
-int main(void) //Main program
+#define BUTTON_0	BIT4
+#define BUTTON_1	BIT5
+#define LED_0		BIT1
+#define LED_1		BIT2
+#define LED_RED		BIT0
+#define LED_GREEN	BIT6
+
+volatile int gTicks = 0;
+
+int main(void)
 {
-	WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+	// Zatrzymanie watchdog timera
+	WDTCTL = WDTPW + WDTHOLD;
 
-	// set up the clocks for the factory preset 1MHz
+	// Ustawienie zegarow na domyslne 1MHz
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
 
-	/* P1DIR |= BIT0; // Set P1.0 to output and P1.3 to input direction
-	 // P1DIR |= BIT1; // Set P1.0 to output and P1.3 to input direction
-	 //P1OUT |= BIT0; // set P1.0 to Off
-	 // P1OUT |= BIT1; // set P1.0 to Off
-	 P1IE |= BIT1; // P1.3 interrupt enabled
-	 P1IFG &= ~BIT1; // P1.3 interrupt flag cleared
-	 P1REN |= BIT1 ;// P1.3 Enable Pull up / Pull down
-	 P1OUT = BIT1 ;// P1.3 pull up
-	 // P1OUT |= BIT1; // set P1.0 to Off
-	 */
+	// Ustawienie wszystkich wyjsc domyslnie jako wejsciowe, wyzerowanie wszystkich pinow oraz flag przerwan.
+	P1DIR = P2DIR = 0;
+	P1OUT = P2OUT = 0;
+	P1IFG = P2IFG = 0;
 
-	P1DIR = BIT1 + BIT2;
-	P1OUT = BIT1 + BIT2;
+	// Zapalenie diod na plytce (ustawienie pinow jako wyjsciowych + wyslanie jedynek na te piny).
+	P1DIR |= LED_RED + LED_GREEN;
+	P1OUT |= LED_RED;
 
-	P2IE |= BIT4; // P1.3 interrupt enabled
-	P2IFG &= ~BIT4; // P1.3 interrupt flag cleared
-	P2REN |= BIT4; // P1.3 Enable Pull up / Pull down
-	P2OUT |= BIT4; // P1.3 pull up
+	// Ustawienie diod LED na pinach 1.1 i 1.2 (ustawienie pinow jako wyjsciowych + wyslanie jedynek na te piny).
+	P1DIR |= LED_0 + LED_1;
+	P1OUT |= LED_0 + LED_1;
 
-	P2IE |= BIT5; // P1.3 interrupt enabled
-	P2IFG &= ~BIT5; // P1.3 interrupt flag cleared
-	P2REN |= BIT5; // P1.3 Enable Pull up / Pull down
-	P2OUT |= BIT5; // P1.3 pull up
+	// Ustawienie przyciskow na pinach 2.4 i 2.5 (wlaczenie przerwan, [?]Enable Pull up / Pull down, [?]pull up).
+	P2IE  |= BUTTON_0 + BUTTON_1;
+	P2REN |= BUTTON_0 + BUTTON_1;
+	P2OUT |= BUTTON_0 + BUTTON_1;
 
+	// [?]Prawdopodobnie czesc z ponizszych jest zbedna - chociazby dlatego, ze wyrzucilem obsluge przerwan NMI (czymkolwiek to nie jest...).
 	// Enable NMI function of the RST pin
 	// WDTTMSEL = Interval timer mode
 	// clock source is SMCLK (1MHz or 1000000Hz)
 	// Interval is clock /64 (15625 interrupts per second)
 	WDTCTL = WDTPW + WDTNMI + WDTTMSEL + WDTCNTCL + WDTIS1 + WDTIS0;
 	IE1 |= WDTIE;
-	__bis_SR_register(LPM1_bits + GIE);
-	// Enable all interrupts
 
-	while (1) //Loop forever, we'll do our job in the interrupt routine...
+	// Wlaczenie wszystkich przerwan
+	__bis_SR_register(GIE);
+
+	// Petla nieskonczona, wszystko bedzie sie dzialo wewnatrz procedur odpalanych przez przerwania
+	while (1)
 	{
+		;
 	}
 }
 
+// Przerwanie odpalane 15625 razy na sekunde.
 #pragma vector=WDT_VECTOR
 __interrupt void wdt_trigger(void)
 {
-// WDT is triggered 15625 times per second
-	ticks++;
+	gTicks++;
 
-		if (ticks >= 15635) {
-			P1OUT ^= BIT1;
-			ticks = 0;
-		}
+	if (gTicks >= 15635)
+	{
+		P1OUT ^= LED_GREEN;
+		P1OUT ^= LED_RED;
+		gTicks = 0;
+	}
 }
 
+// Przerwanie odpalane po kliknieciu dowolnego przycisku z portu 2
 #pragma vector=PORT2_VECTOR
-__interrupt void Port_1(void) {
-	unsigned char flags = P2IFG & P2IE;
+__interrupt void Port_1(void)
+{
 
-	if (flags & BIT4) {
-		P1OUT ^= BIT1; // Toggle P1.0
-		P2IFG &= ~BIT4; // P1.3 interrupt flag cleared
-		P2IFG &= ~BIT5; // P1.3 interrupt flag cleared
-		return;
+	if (P2IFG & BUTTON_0)
+	{
+		// Wyczyszczenie flagi przerwania [?]niekoniecznie dobre, pewnie powinienem czysci tylko odpowiedni bit
+		P2IFG = 0;
+		P1OUT ^= LED_0;
 	}
 
-	if (flags & BIT5) {
-		P1OUT ^= BIT2; // Toggle P1.0
-		P2IFG &= ~BIT5; // P1.3 interrupt flag cleared
+	if (P2IFG & BUTTON_1)
+	{
+		P2IFG = 0;
+		P1OUT ^= LED_1;
 	}
 
 }
