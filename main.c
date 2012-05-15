583b2a03368143da567bb4229a17c813607b80a6
Marek Sklodowski <masklod@gmail.com>
2012-05-15 02:08:07 +0200

    Two buttons switching LEDs

--- main.c
+++ main.c
@@ -4,21 +4,52 @@ int main(void)  //Main program
 {
    WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
 
-   P1DIR |= BIT0; // Set P1.0 to output and P1.3 to input direction
-   P1OUT |= BIT0; // set P1.0 to Off
-   P1IE |= BIT3; // P1.3 interrupt enabled
-   P1IFG &= ~BIT3; // P1.3 interrupt flag cleared
-   P1REN |= BIT3 ;// P1.3 Enable Pull up / Pull down
-   //P1OUT = BIT3 ;// P1.3 pull up
+  /* P1DIR |= BIT0; // Set P1.0 to output and P1.3 to input direction
+  // P1DIR |= BIT1; // Set P1.0 to output and P1.3 to input direction
+   //P1OUT |= BIT0; // set P1.0 to Off
+  // P1OUT |= BIT1; // set P1.0 to Off
+   P1IE |= BIT1; // P1.3 interrupt enabled
+   P1IFG &= ~BIT1; // P1.3 interrupt flag cleared
+   P1REN |= BIT1 ;// P1.3 Enable Pull up / Pull down
+   P1OUT = BIT1 ;// P1.3 pull up
+  // P1OUT |= BIT1; // set P1.0 to Off
+*/
+
+   P1DIR = BIT1 + BIT2;
+   P1OUT = BIT1 + BIT2;
+
+   P2IE |= BIT4; // P1.3 interrupt enabled
+   P2IFG &= ~BIT4; // P1.3 interrupt flag cleared
+   P2REN |= BIT4 ;// P1.3 Enable Pull up / Pull down
+   P2OUT |= BIT4;// P1.3 pull up
+
+   P2IE |= BIT5; // P1.3 interrupt enabled
+   P2IFG &= ~BIT5; // P1.3 interrupt flag cleared
+   P2REN |= BIT5 ;// P1.3 Enable Pull up / Pull down
+   P2OUT |= BIT5 ;// P1.3 pull up
 
    __bis_SR_register(GIE); // Enable all interrupts
 
    while(1) //Loop forever, we'll do our job in the interrupt routine...
    {}
 }
-#pragma vector=PORT1_VECTOR
+#pragma vector=PORT2_VECTOR
 __interrupt void Port_1(void)
 {
-    P1OUT ^= BIT0;  // Toggle P1.0
-    P1IFG &= ~BIT3; // P1.3 interrupt flag cleared
+	unsigned char flags = P2IFG & P2IE;
+
+	if (flags & BIT4)
+	{
+		P1OUT ^= BIT1;  // Toggle P1.0
+		P2IFG &= ~BIT4; // P1.3 interrupt flag cleared
+		P2IFG &= ~BIT5; // P1.3 interrupt flag cleared
+		return;
+	}
+
+	if (flags & BIT5)
+	{
+		P1OUT ^= BIT2;  // Toggle P1.0
+		P2IFG &= ~BIT5; // P1.3 interrupt flag cleared
+	}
+
 }
